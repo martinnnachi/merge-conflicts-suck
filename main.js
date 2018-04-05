673dd130b8e017519a6a98d45c432e6476dd6a28
martinnnachi <mnnachi@gmail.com>
2018-04-05 19:14:15 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+ Volume in drive C has no label.
+ Volume Serial Number is 6CBC-CDE8
+
+ Directory of C:\Users\nnach\Desktop\coderA\merge-conflicts-suck
+
+05/04/2018  19:09    <DIR>          .
+05/04/2018  19:09    <DIR>          ..
+05/04/2018  19:09                 0 main.js
+               1 File(s)              0 bytes
+               2 Dir(s)  171,241,205,760 bytes free
